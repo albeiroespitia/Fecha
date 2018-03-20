89bf0f4ec635c2d8a7c747d9f9dc6c8123674431
Albeiro <albeiroespitiasierra@gmail.com>
2018-03-20 15:05:55 -0500

    Implement validateDate in constructor

--- fecha.js
+++ fecha.js
@@ -1,21 +1,38 @@
 class Fecha{
 	constructor(day,month,year){
-		this.day = day;
-		this.month = month;
-		this.year = year; 
+		if(this.validateDate(`${day}/${month}/${year}`)){
+			this.day = day;
+			this.month = month;
+			this.year = year; 
+		}else{
+			console.log("fecha no es valida")
+		}
+		
 	}
 
 	get day () {
-        return this.day
+        return this._day
     }
 
     get month () {
-        return this.month
+        return this._month
     }
 
     get year () {
-        return this.year
+        return this._year
     }
+
+    set day(value) {
+	    this._day = value;
+	}
+
+	set month(value) {
+	    this._month = value;
+	}
+
+	set year(value) {
+	    this._year = value;
+	}
 
     leapYear(){
     	if ((((this.year%100)!=0)&&((this.year%4)==0))||((this.year%400)==0)){
@@ -29,8 +46,12 @@ class Fecha{
 
     }
 
-    validateDate(){
-
+    validateDate(fecha){
+	    var fechaf = fecha.split("/");
+        var d = fechaf[0];
+        var m = fechaf[1];
+        var y = fechaf[2];
+        return m > 0 && m < 13 && y > 0 && y < 32768 && d > 0 && d <= (new Date(y, m, 0)).getDate();
     }
 
 
