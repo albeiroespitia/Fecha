1f58a0d852c1620499ed399ac04b5e8a960a72d3
Albeiro <albeiroespitiasierra@gmail.com>
2018-03-20 14:39:22 -0500

    Declare Methods

---
+++ fecha.js
@@ -0,0 +1,33 @@
+class Fecha{
+	constructor(day,month,year){
+		this.day = day;
+		this.month = month;
+		this.year = year; 
+	}
+
+	get day () {
+        return this.day
+    }
+
+    get month () {
+        return this.month
+    }
+
+    get year () {
+        return this.year
+    }
+
+    leapYear(){
+
+    }
+
+    plus7Days(){
+
+    }
+
+    validateDate(){
+    	
+    }
+
+
+}
